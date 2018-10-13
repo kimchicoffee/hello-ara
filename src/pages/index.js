7e1c07bbfe6fd5e727d612f74a857a4f66bffdac
Progenitus <progenitus@Progenituss-MacBook-Pro.local>
2018-10-13 19:58:16 -0400

    Add typing effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,59 @@
-import React from "react"
+import React from "react";
+import styled, { keyframes } from "styled-components";
 
-export default () => <div>Hello Ara!</div>
+const blinkCaret = keyframes`
+{ 50% { border-color: transparent; } }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledHello = styled.h1`
+  text-alight: center;
+  font: 300% Consolas, Monaco, monospace;
+  border-right: 0.1rem solid black;
+  margin: 2rem 1rem;
+  animation: ${blinkCaret} 0.75s step-end infinite;
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+  }
+`;
+
+class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { quote: "", quoteArray: "Hello, 아라!".split("") };
+  }
+
+  type() {
+    let char = this.state.quoteArray[0];
+    if (this.state.quoteArray.length > 0) {
+      this.setState(prevState => ({
+        quote: prevState.quote + char,
+        quoteArray: prevState.quoteArray.splice(1)
+      }));
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.quoteArray.length > 0) {
+      this.interval = setInterval(() => this.type(), 500);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <StyledHello> {this.state.quote}</StyledHello>;
+  }
+}
+
+export default () => (
+  <Wrapper>
+    <Hello />
+  </Wrapper>
+);
