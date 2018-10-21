d649c201856069546e174ddc76fb03ea8584be4b
Progenitus <progenitus@Progenituss-MacBook-Pro.local>
2018-10-21 17:41:37 -0400

    Add spinning seed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react";
-import styled, { injectGlobal } from "styled-components";
+import styled, { injectGlobal, keyframes } from "styled-components";
 import Message from "./Message.jsx";
 import Panel from "./Panel.jsx";
 import Hello from "./Hello.jsx";
+import SeedSVG from "./Seed.jsx";
+const isBrowser = typeof window !== "undefined";
+const ScrollMagic = isBrowser ? require("scrollmagic") : undefined;
 
 injectGlobal`
   html, body, #___gatsby{
@@ -17,36 +20,52 @@ injectGlobal`
   }
 `;
 
+const spiningSeed = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`;
+
+const Seed = styled(SeedSVG)`
+  position: relative;
+  width: 300px;
+  height: 300px;
+  top: 20%;
+  animation: ${spiningSeed} infinite 4000ms linear;
+`;
+
 const Wrapper = styled.div``;
 
 export default class Scroll extends React.Component {
   constructor(props) {
     super(props);
-    const isBrowser = typeof window !== "undefined";
-    const ScrollMagic = isBrowser ? require("scrollmagic") : undefined;
+
     if (ScrollMagic) {
       this.controller = new ScrollMagic.Controller({
         globalSceneOptions: {
           triggerHook: "onLeave"
         }
       });
-      const slides = document.querySelectorAll(".panel");
-      // create scene for every slide
-      for (var i = 0; i < slides.length; i++) {
-        new ScrollMagic.Scene({
-          triggerElement: slides[i]
-        })
-          .setPin(slides[i])
-          .addIndicators() // add indicators (requires plugin)
-          .addTo(this.controller);
-      }
     }
   }
+
+  componentDidMount() {
+    const slides = document.querySelectorAll(".panel");
+    // create scene for every slide
+    for (let i = 0; i < slides.length; i++) {
+      new ScrollMagic.Scene({
+        triggerElement: slides[i]
+      })
+        .setPin(slides[i])
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(this.controller);
+    }
+  }
+
   render() {
     return (
       <article>
-        <Panel className="panel" background="black" color="white">
-          <Message>404 Not found</Message>
+        <Panel className="panel" background="white" color="red">
+          <Seed className="seed" />
         </Panel>
         <Panel className="panel" background="white" color="black">
           <Message>TWO</Message>
